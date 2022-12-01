225739f9ec4ce83ac402293605b0a1d970992d8d
lkeleti <keleti.laszlo@vipmail.hu>
2022-12-01 09:20:17 +0100

    Completed both part

--- day01/advent-of-code-day01/src/main/java/dev/lkeleti/Main.java
+++ day01/advent-of-code-day01/src/main/java/dev/lkeleti/Main.java
@@ -1,11 +1,23 @@
 package dev.lkeleti;
 
 import java.nio.file.Path;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
         Service service = new Service();
-        service.readInput(Path.of("/resources/input.txt"));
-        System.out.println(service.sumCalories.stream().max((o1, o2) -> o1.compareTo(o2)));
+        service.readInput(Path.of("src/main//resources/input.txt"));
+
+        //The sorting only need for part 2
+        service.sumCalories.sort(Comparator.reverseOrder());
+
+        System.out.println("Answer of part 1:");
+        System.out.println(service.sumCalories.stream().max(Comparator.naturalOrder()).orElseThrow(
+                ()-> new IllegalArgumentException("Something went wrong!")
+        ));
+
+        Long topThreeCalories = service.sumCalories.get(0) + service.sumCalories.get(1) + service.sumCalories.get(2);
+        System.out.println("Answer of part 2:");
+        System.out.println(topThreeCalories);
     }
 }
