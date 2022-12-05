a5b52026dd8a8cd02d42c6e0ba38086bce77bb71
lkeleti <keleti.kaszlo@vipmail.hu>
2022-12-05 08:07:04 +0100

    Day05 init

---
+++ day05/advent-of-code-day05/readme.md
@@ -0,0 +1,2 @@
+original source: [https://adventofcode.com/2022/day/5](https://adventofcode.com/2022/day/5)
+## --- Day 5:  ---
---
+++ day05/advent-of-code-day05/src/main/java/dev/lkeleti/Main.java
@@ -0,0 +1,8 @@
+package dev.lkeleti;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Answer of part 1:");
+        System.out.println("Answer of part 2:");
+    }
+}
