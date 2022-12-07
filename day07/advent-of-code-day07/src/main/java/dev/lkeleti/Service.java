06f1b9cdc72760c807de72da13b0f412ae6adb9f
lkeleti <keleti.kaszlo@vipmail.hu>
2022-12-07 07:59:43 +0100

    Day07 init

---
+++ day07/advent-of-code-day07/src/main/java/dev/lkeleti/Service.java
@@ -0,0 +1,19 @@
+package dev.lkeleti;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Service {
+    public void readInput(Path path) {
+        try (BufferedReader br = Files.newBufferedReader(path)) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                //receivedData = line;
+            }
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Cannot read file: " + path);
+        }
+    }
+}
