3880fe228a111e308213c75d45617497b5a76c37
lkeleti <keleti.laszlo@vipmail.hu>
2022-12-20 17:16:50 +0100

    Day12 init

--- day20/advent-of-code-day20/src/main/java/dev/lkeleti/Service.java
+++ day20/advent-of-code-day20/src/main/java/dev/lkeleti/Service.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 public class Service {
     private final List<Integer> numbers = new ArrayList<>();
+    private List<Integer> encryptedData;
 
     public void readInput(Path path) {
         try (BufferedReader br = Files.newBufferedReader(path)) {
@@ -19,5 +20,26 @@ public class Service {
         } catch (IOException ioe) {
             throw new IllegalStateException("Cannot read file: " + path);
         }
+    }
+
+    public int decrypt() {
+        encryptedData = new ArrayList<>(numbers);
+        int sizeOfNumbers = numbers.size();
+
+        for (int number: numbers) {
+            int defPosition = encryptedData.indexOf(number);
+            encryptedData.remove(defPosition);
+            int nextPosition = defPosition + number;
+            if (number < 0) {
+                nextPosition = -number % (sizeOfNumbers - 1);
+                nextPosition = (sizeOfNumbers - 1) - nextPosition + 1;
+            } else {
+                nextPosition = nextPosition % (sizeOfNumbers - 1);
+            }
+
+            encryptedData.add(nextPosition, number);
+        }
+
+        return encryptedData.get(1000 % (sizeOfNumbers -1 )) + encryptedData.get(2000 % (sizeOfNumbers -1 )) + encryptedData.get(3000 % (sizeOfNumbers -1 ));
     }
 }
