c274b54f1e4a9f611b03a1630ec0772c0b76616f
lkeleti <keleti.kaszlo@vipmail.hu>
2022-12-20 08:07:03 +0100

    Day20 init

---
+++ day20/advent-of-code-day20/readme.md
@@ -0,0 +1,2 @@
+original source: [https://adventofcode.com/2022/day/20](https://adventofcode.com/2022/day/20)
+## --- Day 20:  ---
---
+++ day20/advent-of-code-day20/src/main/java/dev/lkeleti/Service.java
@@ -0,0 +1,23 @@
+package dev.lkeleti;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Service {
+    private final List<Integer> numbers = new ArrayList<>();
+
+    public void readInput(Path path) {
+        try (BufferedReader br = Files.newBufferedReader(path)) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                numbers.add(Integer.parseInt(line.trim()));
+            }
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Cannot read file: " + path);
+        }
+    }
+}
