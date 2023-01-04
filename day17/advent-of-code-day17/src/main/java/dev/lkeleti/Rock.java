16c9016c4532154b2f3ca6774f1bdc532e81b901
lkeleti <keleti.kaszlo@vipmail.hu>
2023-01-04 22:21:39 +0100

    Day17 init

--- day17/advent-of-code-day17/src/main/java/dev/lkeleti/Rock.java
+++ day17/advent-of-code-day17/src/main/java/dev/lkeleti/Rock.java
@@ -2,6 +2,31 @@ package dev.lkeleti;
 
 public class Rock {
     private static int nextShape = 0;
+    private String[] shape;
+    private int posY;
+    private final int height;
+
+    public Rock(int posY) {
+        this.posY = posY;
+        shape = generate();
+        height = shape.length;
+    }
+
+    public String[] getShape() {
+        return shape;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
 
     public static String[] generate() {
         String[] shape;
@@ -37,5 +62,64 @@ public class Rock {
             nextShape++;
         }
         return shape;
+    }
+
+    public boolean isCollide(Rock otherRock) {
+        if (posY < 0) {
+            return false;
+        }
+
+        if (posY > otherRock.getPosY() + otherRock.getHeight() ||
+                posY + height < otherRock.getPosY()) {
+            return false;
+        }
+
+        int minY = Math.min(posY, otherRock.getPosY());
+        int maxY = Math.max(posY+height, otherRock.getPosY()+ otherRock.getHeight());
+
+        for (int i = minY; i < maxY; i++) {
+            if (posY >= i && posY <= i) {
+                int otherIndex = i - otherRock.getPosY() ;
+                if (otherIndex >= 0 && otherIndex < otherRock.getHeight()) {
+                    String thisRow = shape[i -posY];
+                    String otherRow = otherRock.getShape()[i -otherRock.getPosY()];
+
+                    for ( int j = 0; j < 7; j++) {
+                        if (thisRow.charAt(j) == '#' && otherRow.charAt(j) == '#') {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean canMoveSide(char direction) {
+        int position = 6;
+        if (direction == '<') {
+            position = 0;
+        }
+
+        for (int i = 0; i < height; i++) {
+            if (shape[i].charAt(position) != ' ') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void moveSide(char direction) {
+
+        String[] newShape = new String[height];
+        for (int i = 0; i < height; i++) {
+            if (direction == '<') {
+                newShape[i] = shape[i].substring(1,7) + " ";
+            }
+            else {
+                newShape[i] = " " + shape[i].substring(0,6);
+            }
+        }
+        shape = newShape;
     }
 }
--- day17/advent-of-code-day17/src/main/java/dev/lkeleti/Service.java
+++ day17/advent-of-code-day17/src/main/java/dev/lkeleti/Service.java
@@ -4,10 +4,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Service {
 
     private char[] jetPattern;
+    private List<Rock> rocks = new ArrayList<>();
     public void readInput(Path path) {
         try (BufferedReader br = Files.newBufferedReader(path)) {
             String line;
@@ -19,25 +22,57 @@ public class Service {
         }
     }
 
-    private long simulate() {
+    public long simulate() {
         int counter = 2022;
         int pushCounter = 0;
+        int maxHeight = 0;
 
         while (counter > 0 ) {
-            // max height of board + 4 the start height
+            maxHeight = rocks.stream().mapToInt(r->r.getPosY()+r.getHeight()).max().orElse(0) + 4;
+            Rock defaultRock = new Rock(maxHeight);
             boolean canMove = true;
             while (canMove) {
-                // push
+                if (!defaultRock.canMoveSide(jetPattern[pushCounter])) {
+                    defaultRock.moveSide(jetPattern[pushCounter]);
+                }
+                boolean isCollide = false;
+                for(Rock otherRock : rocks) {
+                    if (defaultRock.isCollide(otherRock)) {
+                        if ( jetPattern[pushCounter] == '>') {
+                            defaultRock.moveSide('<');
+                        }
+                        else {
+                            defaultRock.moveSide('>');
+                        }
+                    }
+                    break;
+                }
+
                 if (pushCounter == jetPattern.length - 1) {
                     pushCounter = 0;
                 } else {
                     pushCounter++;
                 }
-                //moveDown, if can"t move then
-                canMove = false;
+
+                defaultRock.setPosY(defaultRock.getPosY() - 1);
+                if (!rocks.isEmpty()) {
+                    for (Rock otherRock : rocks) {
+                        if (defaultRock.isCollide(otherRock)) {
+                            defaultRock.setPosY(defaultRock.getPosY() + 1);
+                            canMove = false;
+                            break;
+                        }
+                    }
+                }
+                else {
+                    if (defaultRock.getPosY() == 0) {
+                        canMove = false;
+                    }
+                }
             }
             counter--;
+            rocks.add(defaultRock);
         }
-        return 0;
+        return rocks.stream().mapToInt(r->r.getPosY()+r.getHeight()).max().getAsInt();
     }
 }
